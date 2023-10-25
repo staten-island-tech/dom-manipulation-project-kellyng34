64c928e16051835eb96026fe2e05f7545f148a91
Kelly Ng <kellyng19lover@gmail.com>
2023-10-25 10:15:34 -0400

    ey
    ey

--- app.js
+++ app.js
@@ -10,22 +10,30 @@ const DOMSelectors= {
     lastNameoutput: document.querySelector("#lastNameoutput"),
     classoutput: document.querySelector("#classoutput"),
     picoutput: document.querySelector("#picoutput"),
-    //select the text box
-    //select all the h2 in one property
+    box: document.querySelector("#box"),
+    button: document.querySelector("#button")
 };
 /* console.log(DOMSelectors.firstName); */
-DOMSelectors.form.addEventListener("submit", function(event){
-    event.preventDefault();
+function create(){
+
+    const Profile
     const firstName = DOMSelectors.firstName.value
     const lastName = DOMSelectors.lastName.value
     const student_class = DOMSelectors.class.value
     const pic= DOMSelectors.pic.value
+    return {}
+};
+    function divCreater(){
+    form.insertAdjacentHMTL(
+        "afterend",
+        "<div id=box><p id=firstNameoutput></p><p id=lastNameoutput></p><img id=picoutput></img</div><button id=button>Clear Profile</button"
+    );
+
+
     DOMSelectors.firstNameoutput.innerTexts = firstName
     DOMSelectors.lastNameoutput.innerText = lastName
     DOMSelectors.classoutput.innerText = student_class
     document.getElementById("picoutput").src = pic
 
-});
-function clearcontext(DOMSelectors){
-    DOMSelectors.box.interHMTL = "";
-}
+}; 
+
