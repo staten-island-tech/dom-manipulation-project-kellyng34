28e64fa618c388727853ad0a465a2998b7fe288d
Kelly Ng <kellyng19lover@gmail.com>
2023-10-27 10:06:46 -0400

    il
    il

--- app.js
+++ app.js
@@ -4,35 +4,32 @@ const DOMSelectors = {
     form: document.querySelector("#form"),
     firstName: document.querySelector("#firstName"),
     lastName: document.querySelector("#lastName"),
-    class: document.querySelector("#class"),
+    class: document.querySelector("#student_class"),
     pic: document.querySelector("#pic"),
     firstNameoutput: document.querySelector("#firstNameoutput"),
     lastNameoutput: document.querySelector("#lastNameoutput"),
     classoutput: document.querySelector("#classoutput"),
     picoutput: document.querySelector("#picoutput"),
     box: document.querySelector("#box"),
-    button: document.querySelector("#button")
+    button: document.querySelector("#button"),
 };
 /* console.log(DOMSelectors.firstName); */
-function create() {
 
-    const Profile = {
-        firstName: DOMSelectors.firstName.value,
-        lastName: DOMSelectors.firstName.value,
-        class: DOMSelectors.class.value,
-        pic: DOMSelectors.pic.value
-    }
-    return Profile
-};
-function divCreater() {
-    form.insertAdjacentHMTL(
+DOMSelectors.form.addEventListener("submit", function(event){
+    event.preventDefault();
+    const firstName = DOMSelectors.firstName.value
+    const lastName = DOMSelectors.lastName.value
+    const student_class = DOMSelectors.class.value
+    const pic = DOMSelectors.pic.value
+    function divCreater(){
+        card.insertAdjacentHTML(
         "afterend",
-        `<div id=box><p id=firstNameoutput>${firstName}</p><p id=lastNameoutput>${lastName}</p><img id=picoutput>${pic}</img</div><button id="btn">Clear Profile</button>`
-    );
+        `<div id=box><p id=firstNameoutput>${firstName}</p><p id=lastNameoutput>${lastName}</p><p id=classoutput>${student_class}</p><img id=picoutput>${pic}</img</div><button id="button">Clear Profile</button>`
+        );
 
-}
+    }});
 
-divCreater();
+    divCreater();
 function output(firstName, lastName, pic) {
     document.getElementById("firstNameoutput").innerText = firstName
     document.getElementById("lastNameoutput").innerText = lastName
@@ -40,5 +37,14 @@ function output(firstName, lastName, pic) {
 }
 output(Profile.firstName, Profile.lastName, Profile.pic)
 
-};
+
+function remove(){
+    let btns = document.querySelectorAll("button");
+     btns.forEach((button)=> 
+        button.addEventListener("click",function(event) {
+      })
+    );
+}
+remove();
+
 
